138275df4ed33d70c691b366c220f4f7f1019193
Edouard Berthe <berthe.ed@gmail.com>
2020-10-31 12:19:34 +0100

    Add migration to Identity User

--- Migrations/20201030231941_AddIdentity.Designer.cs
+++ Migrations/20201030231941_AddIdentity.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using AspNetCoreUserDataAuthorization.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AspNetCoreUserDataAuthorization.Migrations
 {
     [DbContext(typeof(AspNetCoreUserDataAuthorizationContext))]
-    partial class AspNetCoreUserDataAuthorizationContextModelSnapshot : ModelSnapshot
+    [Migration("20201030231941_AddIdentity")]
+    partial class AddIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
