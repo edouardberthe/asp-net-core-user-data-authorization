5877ce4919b65e046209d0b9e163af65cb4ab099
Edouard Berthe <berthe.ed@gmail.com>
2020-11-01 01:39:11 +0100

    Add seed data

---
+++ Authorization/ContactManager.cs
@@ -0,0 +1,14 @@
+namespace AspNetCoreUserDataAuthorization.Authorization
+{
+    public static class Constants
+    {
+        public const string CreateOperationName = "Create";
+        public const string ReadOperationName = "Read";
+        public const string UpdateOperationName = "Update";
+        public const string DeleteOperationName = "Delete";
+        public const string ApproveOperationName = "Approve";
+        public const string RejectOperationName = "Reject";
+        public const string ContactAdministratorsRole = "ContactAdministrators";
+        public const string ContactManagersRole = "ContactManagers";
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
+using System;
+using AspNetCoreUserDataAuthorization.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace AspNetCoreUserDataAuthorization
@@ -7,7 +12,29 @@ namespace AspNetCoreUserDataAuthorization
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<ApplicationContext>();
+                    Console.WriteLine($"{nameof(context)} has been found: {context}");
+                    context.Database.Migrate();
+
+                    var config = host.Services.GetRequiredService<IConfiguration>();
+                    var testUserPw = config["SeedUserPW"];
+
+                    SeedData.Initialize(services, testUserPw).Wait();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"An error occurred seeding the DB: {e}");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
